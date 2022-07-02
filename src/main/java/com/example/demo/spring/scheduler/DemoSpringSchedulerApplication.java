1b88aeaf6694b9ad8027e19d46dd4c57ed977e7a
amarinos <andy.alexis.marinos.rodriguez@nttdata.com>
2022-07-01 20:50:17 -0500

    initial commit

---
+++ src/main/java/com/example/demo/spring/scheduler/DemoSpringSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.spring.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringSchedulerApplication.class, args);
+	}
+
+}
